c384c1b2ddd70f2e19f0729477639bfdf13d8656
Atanu Ghosh <atanughosh01@users.noreply.github.com>
2021-04-26 22:34:02 +0530

    updated files

--- Coursera_py4e/Course 2: Python Data Structures/Assignments/File_Handling/manual_input.py
+++ Coursera_py4e/Course 2: Python Data Structures/Assignments/File_Handling/manual_input.py
@@ -1,13 +1,14 @@
 
-fname = input('Enter the filename you ant to open = ')
+fname = input('Enter the filename you want to open: ')
 try:
     fhand = open(fname)
 except FileNotFoundError:
-    print('File',fname,'is not found')
+    print('File', fname, 'is not found')
     quit()
 count = 0
+character = input('Enter the first character of the line you want to display: ')
 for line in fhand:
     line = line.strip()
-    if line.startswith('I '):
+    if line.startswith(character):
         count = count+1
-print('There are',count,'subject lines in',fname,'that starts with "I"')
+print('There are', count, 'subject lines in', fname, 'that starts with', character)
--- Coursera_py4e/Course 2: Python Data Structures/Assignments/File_Handling/whole_file.py
+++ Coursera_py4e/Course 2: Python Data Structures/Assignments/File_Handling/whole_file.py
@@ -3,7 +3,7 @@
 fhand = open('readthis.txt')
 inp = fhand.read()
 length = len(inp)
-print('length of text file = ',length)
+print('length of text file = ', length)
 num = input('Enter upto which number, characters to be printed : ')
 pos = int(num)
 print(inp[:pos])

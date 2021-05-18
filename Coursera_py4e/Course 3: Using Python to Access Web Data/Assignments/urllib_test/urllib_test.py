9d84664d2847a43e7a906c2c8357b5acd08273df
Atanu Ghosh <atanughosh01@users.noreply.github.com>
2021-05-19 02:15:16 +0530

    modified urllib_test

--- Coursera_py4e/Course 3: Using Python to Access Web Data/Assignments/urllib_test/urllib_test.py
+++ Coursera_py4e/Course 3: Using Python to Access Web Data/Assignments/urllib_test/urllib_test.py
@@ -1,8 +1,10 @@
 """ for testing the urllib library in python """
 
 import urllib.request, urllib.parse, urllib.error
+
+print("\n----------------------------------------------------\n")
+
 hand = urllib.request.urlopen("http://data.pr4e.org/intro-short.txt")
-print("")
 for line in hand:
     print(line.decode().strip())
 
@@ -15,6 +17,8 @@ for lines in file_hand:
     words = lines.decode().split()
     for word in words:
         counts[word] = counts.get(word, 0) + 1
-print("")
+print("\n----WORD COUNT----\n")
 for x, y in ([(k, v) for k, v in counts.items()]):
     print("Word :", x, ", Count :", y)
+
+print("\n----------------------------------------------------")

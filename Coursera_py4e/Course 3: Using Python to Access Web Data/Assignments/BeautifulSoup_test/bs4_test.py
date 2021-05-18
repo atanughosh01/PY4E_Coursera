ea151fff7abf80c2c663be3cc793d8f05631a902
Atanu Ghosh <atanughosh01@users.noreply.github.com>
2021-05-19 02:23:39 +0530

    modified BeautifulSoup_test

--- Coursera_py4e/Course 3: Using Python to Access Web Data/Assignments/BeautifulSoup_test/bs4_test.py
+++ Coursera_py4e/Course 3: Using Python to Access Web Data/Assignments/BeautifulSoup_test/bs4_test.py
@@ -2,9 +2,15 @@
 
 import urllib.request, urllib.request, urllib.error
 from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 url = input("Enter the URL : ")
-html = urllib.request.urlopen(url).read()
+html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags

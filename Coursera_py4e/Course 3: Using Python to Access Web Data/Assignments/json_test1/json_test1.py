31a42b142f441ec2f1ab94be2ee181ab44fb8298
Atanu Ghosh <atanughosh01@users.noreply.github.com>
2021-05-22 02:11:08 +0530

    modified json_test1

--- Coursera_py4e/Course 3: Using Python to Access Web Data/Assignments/json_test1/json_test1.py
+++ Coursera_py4e/Course 3: Using Python to Access Web Data/Assignments/json_test1/json_test1.py
@@ -23,8 +23,32 @@ data = """ {
 
 info = json.loads(data)
 print("\nINFO :", info)
-print("Type of INFO :", type(info))
-print("Name :", info["name"])
+print("\nType of INFO :", type(info))
+print("\nName :", info["name"])
 print("Email Hidden? :", info["email"]["hide"])
 print("Email is Gmail? :", info["email"]["isGmail"])
 print("PIN :", info["address"]["location"]["pin"])
+
+print("\n----------------------------------------------\n")
+
+msg = """[
+    {
+        "id" : "007",
+        "x" : "01",
+        "name" : "Atanu"
+    },
+    {
+        "id" : "066",
+        "x" : "22",
+        "name" : "Atanu"
+    }
+]"""
+
+msg_info = json.loads(msg)
+print(f"Message Info : {msg_info}")
+print(f"Message Info[0] : {msg_info[0]}\nMessage Info[1] : {msg_info[1]}")
+print("User Count :", len(msg_info))
+for content in msg_info:
+    print("Name :", content["name"])
+    print("ID :", content["id"])
+    print("Attr :", content["x"])

16355720dd04fbc47ecd261ddbf724754fcf742f
Atanu Ghosh <atanughosh01@users.noreply.github.com>
2021-05-22 17:42:06 +0530

    modified json_test2

--- Coursera_py4e/Course 3: Using Python to Access Web Data/Assignments/json_test2/json_test2.py
+++ Coursera_py4e/Course 3: Using Python to Access Web Data/Assignments/json_test2/json_test2.py
@@ -1,51 +1,59 @@
-""" for testing json parsing using json package """
+""" For testing json parsing using json package.
+ Enter "Ann Arbor, MI" as the test location """
 
 import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
 
 api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
 
 if api_key is False:
     api_key = 42
-    service_url = "http://py4e-data.dr-chuck.net/json?"
-else:
-    service_url = "https://maps.googleapis.com/maps/api/geocode/json?"
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-# ignore ssl certificate errors
+# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 while True:
-    address = input("Enter Location : ")
-    if len(address) < 1:
-        break
+    print("\n---------------------------------------------")
+    # Enter "Ann Arbor, MI" as the test location
+    address = input("\nEnter location: ")
+    if len(address) < 1: break
 
     parms = dict()
-    parms["address"] = address
-    if api_key is not False:
-        parms["key"] = api_key
-    url = service_url + urllib.parse.urlencode(parms)
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
 
+    print("Parms :", parms)
+    print("Last portion of URL :", urllib.parse.urlencode(parms))
     print("Retrieving :", url)
+
     data = urllib.request.urlopen(url, context=ctx).read().decode()
-    print(f"Retrieved : {len(data)} characters")
+    print("Retrieved :", len(data), "characters")
 
     try:
         js = json.loads(data)
     except Exception as e:
         js = None
+        print("JSON Load Error :", e)
 
-    if not js or "status" not in js or js["status"] != "ok":
-        print("==== Failure To Retrieve ====")
-        print(data)
+    if not js or "status" not in js or js["status"] != "OK":
+        print("==== Failed To Retrieve ====")
+        print("Data :", data)
         continue
 
-print(json.dumps(js, indent=4))
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    location = js['results'][0]['formatted_address']
 
-lat = js["result"][0]["geometry"]["location"]["lat"]
-lng = js["result"][0]["geometry"]["location"]["lng"]
-print(f"lat {lat} lng {lng}")
-location = js["results"][0]["formatted_address"]
-print(location)
+    print("JSON :", json.dumps(js, indent=4))
+    print("Latitude :", lat, "\nLongitude :", lng)
+    print("Location :", location)

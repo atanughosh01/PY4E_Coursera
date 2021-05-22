45778f20e76d0d505eaf09daafaf4c35d0756032
Atanu Ghosh <atanughosh01@users.noreply.github.com>
2021-05-23 04:04:00 +0530

    modified ex_13_03

--- PY4E/Course 3: Using Python to Access Web Data/Assignments/ex_13_03/ex_13_03.py
+++ PY4E/Course 3: Using Python to Access Web Data/Assignments/ex_13_03/ex_13_03.py
@@ -1,13 +1,47 @@
-""" In this assignment you will write a Python program which will prompt for a location, contact a web service
-and ret rieve JSON for the web service and parse that data, and retrieve the first place_id from the JSON.
-A place ID is a textual identifier that uniquely identifies a place as within Google Maps. To complete this
-assignment, you should use this API endpoint that has a static subset of the Google Data: http://py4e-data.dr-chuck.net/json?
-This API uses the same parameter (address) as the Google API. This API also has no rate limit so you can test as often as
-you like. If you visit the URL with no parameters, you get "No address..." response. To call the API, you need to include
-a key= parameter and provide the address that you are requesting as the address= parameter that is properly URL encoded using the
-urllib.parse.urlencode() function as shown in http://www.py4e.com/code3/geojson.py. Make sure to check that your code is using the
-API endpoint is as shown above. You will get different results from the geojson and json endpoints so make sure you are using the
-same end point as this autograder is using."""
+"""
+CALLING A JSON API
+In this assignment you will write a Python program somewhat similar to
+http://www.py4e.com/code3/geojson.py. The program will prompt for a location,
+contact a web service and retrieve JSON for the web service and parse that data,
+and retrieve the first place_id from the JSON. A place ID is a textual identifier
+that uniquely identifies a place as within Google Maps.
+
+
+API ENDPOINTS
+To complete this assignment, you should use this API endpoint that has a static
+subset of the Google Data:
+
+http://py4e-data.dr-chuck.net/json?
+
+This API uses the same parameters (address) as the Google API. This
+API also has no rate limit so you can test as often as you like.
+If you visit the URL with no parameters, you get "No address..." response.
+
+To call the API, you need to include a key= parameter and the address
+that you are requesting as the address= parameter that is properly URL encoded
+using the urllib.parse.urlencode() fuction as shown in
+http://www.py4e.com/code3/geojson.py
+
+Make sure to check that your code is using the API endpoint is as shown above.
+You will get different results from the geojson and json endpoints so make sure
+you are using the same end point as this autograder is using.
+
+
+TEST DATA / SAMPLE EXECUTION
+You can test to see if your program is working with a location of "South Federal
+University" which will have a place_id of "ChIJLzabHQ7i2IgRzeZb_AgUj0Q".
+
+
+TURN IN
+Please run your program to find the place_id for this location: Portland State University
+
+Make sure to enter the name and case exactly as above and enter the place_id and
+your Python code below. Hint: The first seven characters of the place_id are "ChIJkbA ...".
+
+Make sure to retreive the data from the URL specified above and
+not the normal Google API. Your program should work with the Google API - but the
+place_id may not match for this assignment.
+"""
 
 # import necessary packages
 import urllib.request, urllib.parse, urllib.error
@@ -21,7 +55,7 @@ api_key = False
 
 if api_key is False:
     api_key = 42
-    service_url = "http://py4e-data.dr-chuck.net/json?"
+    service_url = "http://py4e-data.dr-chuck.net/geojson?"
 else:
     service_url = "https://maps.googleapis.com/maps/api/geocode/json?"
 
@@ -31,8 +65,8 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 while True:
-    # Enter "South Federal University" as the address which will have a place_id of "ChIJLzabHQ7i2IgRzeZb_AgUj0Q"
-    address = input("Enter Location (Type S and press ENTER to stop) : ")
+    # Enter "South Federal University" as address which has place_id of "ChIJLzabHQ7i2IgRzeZb_AgUj0Q"
+    address = input("\nEnter Location (Type S and press ENTER to stop) : ")
     if len(address) < 1: break
     if address == "s" or address == "S":
         print("Process is being stopped....Process Stopped")
@@ -44,9 +78,9 @@ while True:
         address_key_pair["api_key"] = api_key
     url = service_url + urllib.parse.urlencode(address_key_pair)
 
-    print("Address and Key :", address_key_pair)
+    print("\nAddress and Key :", address_key_pair)
     print("Last portion of URL :", urllib.parse.urlencode(address_key_pair))
-    print("Retrieving :", url)
+    print("\nRetrieving :", url)
 
     data = urllib.request.urlopen(url, context=ctx).read().decode()
     print(f"Retrieved {len(data)} characters")
@@ -57,5 +91,13 @@ while True:
         info = None
         print(e)
 
-    print("DATA :", data)
+    print("\nDATA :", data)
     print("INFO :", info)
+
+    if not info or "status" not in info or info["status"] != "OK":
+        print("==== Failed To Retrieve ====")
+        print("Data :", data)
+        continue
+
+    place_id = info["results"][0]["place_id"]
+    print(f"\nThe Place ID of {address} is : {place_id}")

23b173a29240b349359de3940eee3c22bd37d933
Atanu Ghosh <atanughosh01@users.noreply.github.com>
2021-05-23 02:48:40 +0530

    modified folder structure

--- Coursera_py4e/Course 2: Python Data Structures/Assignments/File_Handling/file_hand.py
+++
@@ -1,9 +0,0 @@
-
-fhand = open('readthis.txt')
-count = 0
-for line in fhand:
-    line = line.strip()
-    if not line.startswith('I'):
-        print(line)
-    count = count+1
-print('Number of Lines =', count)

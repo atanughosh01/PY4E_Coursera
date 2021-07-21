eba782fc9a1c5a41af33dce011ad6c662dfb78bf
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-07-21 16:20:22 +0530

    added shift2.c

---
+++ C4E/Course 1: C for Everyone-Programming Fundamentals/week-6/shift/shift2.c
@@ -0,0 +1,26 @@
+// C program to understand the function call in a better way
+
+#include <stdio.h>
+
+int func(int);
+
+int main()
+{
+    int val;
+    val = func(435);
+    // Value printed will be?
+    printf("\nValue = %d\n\n", val);
+
+    return 0;
+}
+
+int func(int num)
+{
+    int count = 2;
+    while (num)
+    {
+        count++;
+        num >> 2;
+    }
+    return count;
+}

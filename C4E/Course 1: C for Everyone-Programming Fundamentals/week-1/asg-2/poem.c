753b7a2beeb56d5a39fc42b2c800bcf958ac2794
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-06 02:09:43 +0530

    removed C4E folder

--- C4E/Course 1: C for Everyone-Programming Fundamentals/week-1/asg-2/poem.c
+++
@@ -1,9 +0,0 @@
-// A program to print a poem using printf()
-
-#include<stdio.h>
-
-int main(void)
-{
-	printf("\n#2. Robert Frost – Fire & Ice\n\nSome say the world will end in fire,\nSome say in ice.\nFrom what I’ve tasted of desire\nI hold with those who favour fire.\nBut if it had to perish twice,\nI think I know enough of hate\nTo say that for destruction ice\nIs also great\nAnd would suffice.\n\n");
-	return 0;
-}
--- C4E/Course 1: C for Everyone-Programming Fundamentals/week-6/staticFunc/func1.h
+++
@@ -1,6 +0,0 @@
-static int func(int a, int b)
-{
-    static int c;
-    c = a + b;
-    return c;
-}

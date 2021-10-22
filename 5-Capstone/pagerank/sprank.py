400b47cc4c7b7c45e63a3b86f0ebae63e82a6d2b
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-10-22 18:01:08 +0530

    formatted source code

--- 5-Capstone/pagerank/sprank.py
+++ 5-Capstone/pagerank/sprank.py
@@ -7,21 +7,25 @@ cur = conn.cursor()
 # in pages in the SCC that have in and out links
 cur.execute('''SELECT DISTINCT from_id FROM Links''')
 from_ids = list()
-for row in cur: 
+for row in cur:
     from_ids.append(row[0])
 
-# Find the ids that receive page rank 
+# Find the ids that receive page rank
 to_ids = list()
 links = list()
 cur.execute('''SELECT DISTINCT from_id, to_id FROM Links''')
 for row in cur:
     from_id = row[0]
     to_id = row[1]
-    if from_id == to_id : continue
-    if from_id not in from_ids : continue
-    if to_id not in from_ids : continue
+    if from_id == to_id:
+        continue
+    if from_id not in from_ids:
+        continue
+    if to_id not in from_ids:
+        continue
     links.append(row)
-    if to_id not in to_ids : to_ids.append(to_id)
+    if to_id not in to_ids:
+        to_ids.append(to_id)
 
 # Get latest page ranks for strongly connected component
 prev_ranks = dict()
@@ -32,17 +36,18 @@ for node in from_ids:
 
 sval = input('How many iterations:')
 many = 1
-if ( len(sval) > 0 ) : many = int(sval)
+if (len(sval) > 0):
+    many = int(sval)
 
 # Sanity check
-if len(prev_ranks) < 1 : 
+if len(prev_ranks) < 1:
     print("Nothing to page rank.  Check data.")
     quit()
 
 # Lets do Page Rank in memory so it is really fast
 for i in range(many):
     # print prev_ranks.items()[:5]
-    next_ranks = dict();
+    next_ranks = dict()
     total = 0.0
     for (node, old_rank) in list(prev_ranks.items()):
         total = total + old_rank
@@ -54,18 +59,21 @@ for i in range(many):
         # print node, old_rank
         give_ids = list()
         for (from_id, to_id) in links:
-            if from_id != node : continue
+            if from_id != node:
+                continue
            #  print '   ',from_id,to_id
 
-            if to_id not in to_ids: continue
+            if to_id not in to_ids:
+                continue
             give_ids.append(to_id)
-        if ( len(give_ids) < 1 ) : continue
+        if (len(give_ids) < 1):
+            continue
         amount = old_rank / len(give_ids)
         # print node, old_rank,amount, give_ids
-    
-        for id in give_ids:
-            next_ranks[id] = next_ranks[id] + amount
-    
+
+        for ID in give_ids:
+            next_ranks[ID] = next_ranks[ID] + amount
+
     newtot = 0
     for (node, next_rank) in list(next_ranks.items()):
         newtot = newtot + next_rank
@@ -96,8 +104,7 @@ for i in range(many):
 # Put the final ranks back into the database
 print(list(next_ranks.items())[:5])
 cur.execute('''UPDATE Pages SET old_rank=new_rank''')
-for (id, new_rank) in list(next_ranks.items()) :
-    cur.execute('''UPDATE Pages SET new_rank=? WHERE id=?''', (new_rank, id))
+for (ID, new_rank) in list(next_ranks.items()):
+    cur.execute('''UPDATE Pages SET new_rank=? WHERE id=?''', (new_rank, ID))
 conn.commit()
 cur.close()
-
